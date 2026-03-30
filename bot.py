0af989515e98b21bddcacb53b39ee5ce81f1dab7
pokemaru6 <pokemaru6@gmail.com>
2026-03-30 10:22:43 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-print("24時間放置システム、起動に成功したよ！")
+import requests
+
+def get_site(url):
+    response = requests.get(url)
+    print(f"--- {url} の中身を取得しました ---")
+    print(response.text[:500]) # 最初の500文字だけ表示
+
+# ここに見たいサイトのURLを入れる
+get_site("https://www.google.com")
+
